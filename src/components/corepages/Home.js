4cef57a16dfb6682e0edab9cd8a7fd1509e84519
Joey Byrd <joeybyrd9999@gmail.com>
2022-10-14 16:45:41 -0700

    added homepage

--- src/components/corepages/Home.js
+++ src/components/corepages/Home.js
@@ -1,1 +1,15 @@
-import React from "react";
+import React from 'react';
+import Recipes from '../Recipes/recipes';
+import Today from '../Today/today'
+
+
+function HomePage() {
+    return (
+        <div>
+            <Recipes></Recipes>
+            <Today></Today>
+        </div>
+    );
+}
+
+export default HomePage;
