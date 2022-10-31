b964daf2754832bfce3e9fa51556a8536ca9d69f
Joey Byrd <joeybyrd9999@gmail.com>
2022-10-31 13:12:08 -0700

    finished routes

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || '', {
-    useNewUrlParser: true,
-    useInifiedTopology: true
-});
-
-mongoose.set('debug', true);
