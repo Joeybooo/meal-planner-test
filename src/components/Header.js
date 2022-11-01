f564b9421b64d469e3af259295c8e8d9c8e6efcf
Joey Byrd <joeybyrd9999@gmail.com>
2022-11-01 14:10:35 -0700

    added singup and changed colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,14 @@ export default function Header({
     return(
         <div className="mb-10">
             <div className="flex justify-center">
-                
+                <h1 className='mt-6 text-center font-extrabold text-green-500'> Meal Planner </h1>
             </div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 {heading}
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600 mt-5">
             {paragraph} {' '}
-            <Link to={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+            <Link to={linkUrl} className="font-medium text-green-600 hover:text-purple-500">
                 {linkName}
             </Link>
             </p>
