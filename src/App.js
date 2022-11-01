da40a5a341c1e1ada69a53ae68d76353b479696d
Joey Byrd <joeybyrd9999@gmail.com>
2022-11-01 13:47:26 -0700

    added components but app will not show css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import Homepage from './components/homepage/Homepage';
-import CorePage from './components/corepages/Core';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Homepage></Homepage>
-      <CorePage></CorePage>
+    <div className='min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+    <div className="max-w-md w-full space-y-8">
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
     </div>
   );
 }
