ed4daf513e258e98f3cc99deaf41e6dbaeba00cb
Joey Byrd <joeybyrd9999@gmail.com>
2022-11-01 16:18:29 -0700

    made it so after login goes to core page

---
+++ src/components/CorePageComponents/Calander.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Calander() {
+    return(
+        <div></div>
+    );
+}
+
+export default Calander;
---
+++ src/components/CorePageComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className="page-footer">
+            <p className="center">© 2022  Timely Meals</p>
+        </footer>
+    )
+}
+
+export default Footer;
