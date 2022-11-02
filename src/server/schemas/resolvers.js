ddc948e00344ca1173dc66cac92219a752c94c28
Joey Byrd <joeybyrd9999@gmail.com>
2022-11-02 09:28:46 -0700

    used graphql to make querries for appolo server

--- src/server/schemas/resolvers.js
+++ src/server/schemas/resolvers.js
@@ -2,9 +2,24 @@ const { User, Recipe } = require('../models');
 
 const resolvers = {
     Query: {
+      users: async () => {
+        return User.find()
+          .select('-_v -password')
+          .populate('friends')
+          .populate('recipes')
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .select('-_v -passowrd')
+          .populate('friends')
+          .populate('recipes')
+      },
       recipes: async () => {
         const params = username ? { username } : {};
         return Recipe.find(params).sort({ createdAt: -1 });
+      },
+      recipe: async (parent, {_id}) => {
+        return Recipe.findOne({_id});
       }
     }
   };
